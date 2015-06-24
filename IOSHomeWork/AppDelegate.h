ece4681d42fb313f21e288e1379eeeef6c334639
taisiyat <taisiyatk@gmail.com>
2015-06-24 15:23:40 +0300

    IOSHomeWork -> creating project

---
+++ IOSHomeWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSHomeWork
+//
+//  Created by Taisiya on 24.06.15.
+//  Copyright (c) 2015 TKAHomeWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSHomeWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOSHomeWork
+//
+//  Created by Taisiya on 24.06.15.
+//  Copyright (c) 2015 TKAHomeWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
