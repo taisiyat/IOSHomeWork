4764c0da188e3a2c2ffb1d2248c08c3645d408a4
taisiyat <taisiyatk@gmail.com>
2015-07-01 16:59:38 +0300

    TKAStudying and button to transform

---
+++ Source/TKAStudyViewController/TKAStudying.h
@@ -0,0 +1,13 @@
+//
+//  TKAStudying.h
+//  IOSHomeWork
+//
+//  Created by Taisiya on 01.07.15.
+//  Copyright (c) 2015 TKAHomeWork. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TKAStudying : NSObject
+
+@end
---
+++ Source/TKAStudyViewController/TKAStudying.m
@@ -0,0 +1,13 @@
+//
+//  TKAStudying.m
+//  IOSHomeWork
+//
+//  Created by Taisiya on 01.07.15.
+//  Copyright (c) 2015 TKAHomeWork. All rights reserved.
+//
+
+#import "TKAStudying.h"
+
+@implementation TKAStudying
+
+@end
