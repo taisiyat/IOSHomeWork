3e68fb45f77876e9f7dcd4cf0993d39f4516193d
taisiyat <taisiyatk@gmail.com>
2015-10-17 19:53:36 +0300

    TKAModel

---
+++ Source/Models/TKAModel/TKAModel.h
@@ -0,0 +1,13 @@
+//
+//  TKAModel.h
+//  IOSHomeWork
+//
+//  Created by Taisiya on 17.10.15.
+//  Copyright (c) 2015 TKAHomeWork. All rights reserved.
+//
+
+#import "TKAObservableObject.h"
+
+@interface TKAModel : TKAObservableObject
+
+@end
---
+++ Source/Models/TKAModel/TKAModel.m
@@ -0,0 +1,13 @@
+//
+//  TKAModel.m
+//  IOSHomeWork
+//
+//  Created by Taisiya on 17.10.15.
+//  Copyright (c) 2015 TKAHomeWork. All rights reserved.
+//
+
+#import "TKAModel.h"
+
+@implementation TKAModel
+
+@end
